d600528e6ef00bda331b96c08d64137e405b9f4a
Gabriel Anesi de Oliveira <gabriel.anesi@gmail.com>
2022-10-14 14:17:35 -0300

    mais funcionalidades no modal

--- 01 - Compra de pizzas/script.js
+++ 01 - Compra de pizzas/script.js
@@ -3,7 +3,31 @@ const cs = (el)=>document.querySelectorAll(el); //selecionar o array
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+    
+    pizzaItem.setAttribute('data-key',index);//identificando cada pizza
     //preencher as informações
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    //configurando o modal
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
+        //animação pra abrir o modal mais suavemente
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
+    
+    
 
     c('.pizza-area').append(pizzaItem);
 
